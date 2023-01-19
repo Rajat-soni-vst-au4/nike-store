4ae56ae47dd7889013067c15374263364b29369d
rajat-soni-vst-au4 <rajatsoni.rs66@gmail.com>
2023-01-19 15:05:04 +0530

    contanier item added

--- src/components/utils/Item.jsx
+++ src/components/utils/Item.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import React from "react";
+import { useDispatch } from "react-redux";
 
-const Item = () => {
+import { StarIcon, ShoppingBagIcon } from "@heroicons/react/24/solid";
+import { setAddItemToCart, setOpenCart } from "../../app/CartSlice";
+
+const Item = ({
+  ifExists,
+  id,
+  color,
+  shadow,
+  title,
+  text,
+  img,
+  btn,
+  rating,
+  price,
+}) => {
+  //   console.log(id)
+  const dispatch = useDispatch();
+
+  const onAddToCart = () => {
+    const item = { id, title, text, img, color, shadow, price };
+
+    dispatch(setAddItemToCart(item));
+  };
+
+  const onCartToggle = () => {
+    dispatch(
+      setOpenCart({
+        cartState: true,
+      })
+    );
+  };
+
   return (
-    <div>Item</div>
-  )
-}
+    <>
+      <div
+        className={`relative bg-gradient-to-b ${color} ${shadow} grid items-center ${
+          ifExists ? "justify-items-start" : "justify-items-center"
+        } rounded-xl py-4 px-5 transition-all duration-700 ease-in-out w-full hover:scale-105`}
+      >
 
-export default Item
+      </div>
+    </>
+  );
+};
+
+export default Item;
